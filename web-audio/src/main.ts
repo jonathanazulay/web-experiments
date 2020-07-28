18b900595f43a03504a03abb0cd13299225c3d22
Jonathan Azulay <jonathanazulay@users.noreply.github.com>
2020-07-28 23:36:26 +0200

    control bg color with knobs, wow

--- web-audio/src/main.ts
+++ web-audio/src/main.ts
@@ -1,3 +1,5 @@
+import './style.css'
+
 const deviceName = "Elektron Digitakt Digitakt in 1"
 
 navigator['requestMIDIAccess']().then(withMidi)
@@ -18,8 +20,22 @@ function uint8arrayToBinary (arr) {
   return [...arr].map(a => a.toString(2).padStart(8, '0'))
 }
 
+let r = 0;
+let g = 240;
+let b = 140;
 
 function withMessage (m) {
+  if (m.data[0] === 192) {
+    r = Math.sin(m.data[1]/127)*255*0.5+255*0.5
+  }
+  if (m.data[0] === 208) {
+    g = Math.sin(m.data[1]/127)*255*0.5+255*0.5
+  }
+  if (m.data[0] === 176) {
+    b = Math.sin(m.data[2]/127)*255*0.5+255*0.5
+  }
+
+  document.querySelector(':root').style.setProperty('--bg-color', `rgb(${r}, ${g}, ${b})`)
   console.table({
     ...uint8arrayToBinary(m.data),
     timestamp: m.timeStamp,
